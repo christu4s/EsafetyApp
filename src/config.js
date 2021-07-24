084829c0e171d5501e5aa1f11d7415ae9aec08af
unknown <balakishore.krish@gmail.com>
2021-07-24 18:14:35 +0530

    Emergency Response pages added

--- src/config.js
+++ src/config.js
@@ -2,32 +2,37 @@
 import { ShopOutlined, HomeFilled } from '@ant-design/icons';
 import { UserGroupFilled } from './icons';
 
-import { AccidentsHazard, Dashboard, Facilities, FacilityArea, FacilityManning, FacilityProcess, RiskAssessment, HazardIdentifications, HazardOperatability, HazardAnalysis, HazardEmergency, CriticalEquipment, CriticalPersonnel, CriticalProcedure, CriticalElement, SelectAccount, EquipmentPrevention } from './views';
+import { AccidentsHazard, Dashboard, Facilities, FacilityArea, FacilityManning, FacilityProcess, RiskAssessment, HazardIdentifications, HazardOperatability, HazardAnalysis, HazardEmergency, CriticalEquipment, CriticalPersonnel, CriticalProcedure, CriticalElement, SelectAccount, EquipmentPrevention, EmergencyResponse, ResponseTiers, ResponseOrganisation, ResponsePlan, ScenarioActionPlan } from './views';
 
 import facilityImg from './assets/menu/blueprint.png';
 
 const Empty = () => <div />;
 
 export const routes = [
-    {path: '/', exact: true ,component: Dashboard},
-    {path: '/select-account',exact: true, component: SelectAccount},
-    {path: '/user',exact: true, component: Empty},
-    {path: '/profile',exact: true, component: Empty},
-    {path: '/facility-overview',exact: true, component: Facilities},
-    {path: '/facility-overview/area',exact: true, component: FacilityArea},
-    {path: '/facility-overview/manning',exact: true, component: FacilityManning},
-    {path: '/facility-overview/process',exact: true, component: FacilityProcess},
-    {path: '/accidents-hazards',exact: true, component: AccidentsHazard},
-    {path: '/risk-assessment',exact: true, component: RiskAssessment},
-    {path: '/risk-assessment/identification-worksheet',exact: true, component: HazardIdentifications},
-    {path: '/risk-assessment/operability-worksheet',exact: true, component: HazardOperatability},
-    {path: '/risk-assessment/rescue-analysis',exact: true, component: HazardAnalysis},
-    {path: '/risk-assessment/survivability-analysis',exact: true, component: HazardEmergency},
-    { path: '/safety-critical', exact:true, component: CriticalElement },
+    { path: '/', exact: true, component: Dashboard },
+    { path: '/select-account', exact: true, component: SelectAccount },
+    { path: '/user', exact: true, component: Empty },
+    { path: '/profile', exact: true, component: Empty },
+    { path: '/facility-overview', exact: true, component: Facilities },
+    { path: '/facility-overview/area', exact: true, component: FacilityArea },
+    { path: '/facility-overview/manning', exact: true, component: FacilityManning },
+    { path: '/facility-overview/process', exact: true, component: FacilityProcess },
+    { path: '/accidents-hazards', exact: true, component: AccidentsHazard },
+    { path: '/risk-assessment', exact: true, component: RiskAssessment },
+    { path: '/risk-assessment/identification-worksheet', exact: true, component: HazardIdentifications },
+    { path: '/risk-assessment/operability-worksheet', exact: true, component: HazardOperatability },
+    { path: '/risk-assessment/rescue-analysis', exact: true, component: HazardAnalysis },
+    { path: '/risk-assessment/survivability-analysis', exact: true, component: HazardEmergency },
+    { path: '/safety-critical', exact: true, component: CriticalElement },
     { path: '/safety-critical/equipment', exact: true, component: CriticalEquipment },
     { path: '/safety-critical/equipment/prevention', exact: true, component: EquipmentPrevention },
     { path: '/safety-critical/personnel', exact: true, component: CriticalPersonnel },
     { path: '/safety-critical/procedure', exact: true, component: CriticalProcedure },
+    { path: '/emergency-response', exact: true, component: EmergencyResponse },
+    { path: '/emergency-response/tiers', exact: true, component: ResponseTiers },
+    { path: '/emergency-response/organisation', exact: true, component: ResponseOrganisation },
+    { path: '/emergency-response/plan', exact: true, component: ResponsePlan },
+    { path: '/emergency-response/scenario', exact: true, component: ScenarioActionPlan },
 ];
 
 export const menus = [
@@ -76,8 +81,8 @@ export const menus = [
             { title: 'Emergency Response Tiers', exact: true, url: '#/emergency-response/tiers' },
             { title: 'Emergency Response Organisation', exact: true, url: '#/emergency-response/organisation' },
             { title: 'Emergency Response Plan', exact: true, url: '#/emergency-response/plan' },
-            { title: 'Scenario Specific', exact: true, url: '#/emergency-response/scenario' },
-            { title: 'Action Plan', exact: true, url: '#/emergency-response/action' },
+            { title: 'Scenario Specific Action Plan', exact: true, url: '#/emergency-response/scenario' },
+            // { title: 'Action Plan', exact: true, url: '#/emergency-response/action' },
         ]
     },
     { title: 'Safety Management System', icon: <UserGroupFilled />, url: '#/safety-management' },
---
+++ src/views/EmergencyResponse/index.js
@@ -0,0 +1,5 @@
+export * from './emergencyResponse';
+export * from './responseTiers';
+export * from './responseOrganisation';
+export * from './responsePlan';
+export * from './scenarioActionPlan'
