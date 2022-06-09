91a55678516ef250737e976601d67cb9ce483eb8
unknown <christu4s@gmail.com>
2022-06-09 19:44:01 +0800

    Datatable and search option is updated

--- src/views/EmergencyResponse/responseOrganisation/index.js
+++ src/views/EmergencyResponse/responseOrganisation/index.js
@@ -1,5 +1,5 @@
-import { Row, Col, Card, Button, Modal, Upload, Input, Form, Space, Popconfirm } from 'antd';
-import React, { useState, useEffect } from 'react';
+import { Row, Col, Card, Button, Modal, Upload, Input, Form, Space, Popconfirm, Table } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
 
 import image from '../../../assets/06107-f-28-fig-3@3x.png';
 import extinguisher from '../../../assets/fire-extinguisher@3x.png';
@@ -8,7 +8,8 @@ import computing from '../../../assets/cloud-computing@3x.png';
 import ajax from '../../../ajax';
 import { Link } from 'react-router-dom';
 import { PageTemplate } from './../../template';
-
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 export const ResponseOrganisation = () => {
     return <PageTemplate
         iconUrl={extinguisher}
@@ -18,13 +19,15 @@ export const ResponseOrganisation = () => {
         descName="organisation_desc"
         imageName="organisation_image"
         pdfName="organisation_pdf"
+        videoName="organisation_video"
     >{(content,editMode,form)=> <TableOrg content={content} editMode={editMode} form={form} />}
     </PageTemplate>
 }
 
 function TableOrg({ content, editMode, form }) {
     const [data, setData] = useState([]);
-
+    const [newData, setNewData] = useState([]);
+    const [result, setResult] = useState([]);
     useEffect(() => {
         try {
             var res = JSON.parse(content.team_members.replace(/\\/g, ''));
@@ -32,50 +35,185 @@ function TableOrg({ content, editMode, form }) {
         } catch (e) { }
     }, [content.team_members]);
 
-    useEffect(() => { form.setFieldsValue({ team_members: JSON.stringify(data) }) }, [data]);
-
+    useEffect(() => { 
+        setResult(data.map((item, i) => Object.assign({}, item, newData)));
+        form.setFieldsValue({ team_members: JSON.stringify(result) }) }, [result]);
+    //useEffect(() => { form.setFieldsValue({ team_members: JSON.stringify(newData) }) }, [newData]);
 
     function removeLevel(index) {
-        data.splice(index, 1);
-        setData([...data]);
+        newData.splice(index, 1);
+        setNewData([...newData]);
     }
 
     function onLevelChange(index, key, value) {
-        data[index][key] = value;
-        setData([...data]);
+        newData[index][key] = value;
+        setNewData([...newData]);
     }
-    function addmore() { setData([...data, {}]); }
+    function addmore() { setNewData([...newData, {}]); }
+    // datatable search fuctionality
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+  
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      setSearchText(selectedKeys[0]);
+      setSearchedColumn(dataIndex);
+    };
+  
+    const handleReset = (clearFilters) => {
+      clearFilters();
+      setSearchText('');
+    };
+  
+    const getColumnSearchProps = (dataIndex) => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div
+          style={{
+            padding: 8,
+          }}
+        >
+          <Input
+            ref={searchInput}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{
+              marginBottom: 8,
+              display: 'block',
+            }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              icon={<SearchOutlined />}
+              size="small"
+             
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => clearFilters && handleReset(clearFilters)}
+              size="small"
+              style={{
+                width: 90,
+                height:'40px'
+              }}
+            >
+              Reset
+            </Button>
+            
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined
+          style={{
+            color: filtered ? '#1890ff' : undefined,
+          }}
+        />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+      onFilterDropdownVisibleChange: (visible) => {
+        if (visible) {
+          setTimeout(() => searchInput.current?.select(), 100);
+        }
+      },
+      render: (text) =>
+        searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{
+              backgroundColor: '#ffc069',
+              padding: 0,
+            }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
+    const columns = [
+        {
+            title: 'Team Members',
+            dataIndex: 'teamMembers',
+            key: 'teamMembers',
+            ...getColumnSearchProps('teamMembers'),
+          },
+        {
+          title: 'Roles and responsibility',
+          dataIndex: 'roles',
+          key: 'roles',
+          ...getColumnSearchProps('roles'),
+        }
+       
+        
+      ];
 
-
+    // return <div className='box--facility bg-white-box societal-risk-table remedial-action-plan manning--box--facility'>
+    //     <Form.Item hidden name="team_members"><Input /></Form.Item>
+    //     <Row gutter={20}>
+    //         <Col span={12} >
+    //             <h3>Team Member</h3>
+    //         </Col>
+    //         <Col span={12}>
+    //             <h3>Roles and Responbilities</h3>
+    //         </Col>
+    //     </Row>
+    //     <hr />
+    //     {data.map((team, index) => <>
+    //         <Row gutter={16}>
+    //             <Col span={10}>
+    //                 <Input placeholder="1" readOnly={!editMode} value={team.teamMembers} onChange={e => onLevelChange(index, 'teamMembers', e.target.value)} />
+    //             </Col>
+    //             <Col span={10}>
+    //                 <Input.TextArea placeholder="10" readOnly={!editMode} value={team.roles} onChange={e => onLevelChange(index, 'roles', e.target.value)} />
+    //             </Col>
+    //             <Col span={2}>
+    //                 {editMode &&
+    //                     <Popconfirm title="Are you sure to delete this level?" onConfirm={() => removeLevel(index)}>
+    //                         <Button type="link" icon={<DeleteOutlined danger />} />
+    //                     </Popconfirm>
+    //                 }
+    //             </Col>
+    //         </Row>
+    //         <hr />
+    //     </>)}
+    //     {editMode &&
+    //         <Row className='addmore--button'>
+    //             <Col>
+    //                 <Button type="default" icon={<PlusCircleOutlined />} onClick={addmore}>
+    //                     Add More
+    //                 </Button>
+    //             </Col>
+    //         </Row>
+    //     }
+    // </div>
     return <div className='box--facility bg-white-box societal-risk-table remedial-action-plan manning--box--facility'>
         <Form.Item hidden name="team_members"><Input /></Form.Item>
-        <Row gutter={20}>
-            <Col span={12} >
-                <h3>Team Member</h3>
-            </Col>
-            <Col span={12}>
-                <h3>Roles and Responbilities</h3>
-            </Col>
-        </Row>
-        <hr />
-        {data.map((team, index) => <>
-            <Row gutter={16}>
+        <Table columns={columns} dataSource={data} />
+        {newData.map((team, index) => <>
+             <Row gutter={16}>
                 <Col span={10}>
-                    <Input placeholder="1" readOnly={!editMode} value={team.teamMembers} onChange={e => onLevelChange(index, 'teamMembers', e.target.value)} />
+                     <Input placeholder="1" readOnly={!editMode} value={team.teamMembers} onChange={e => onLevelChange(index, 'teamMembers', e.target.value)} />
                 </Col>
-                <Col span={10}>
-                    <Input.TextArea placeholder="10" readOnly={!editMode} value={team.roles} onChange={e => onLevelChange(index, 'roles', e.target.value)} />
-                </Col>
-                <Col span={2}>
-                    {editMode &&
-                        <Popconfirm title="Are you sure to delete this level?" onConfirm={() => removeLevel(index)}>
+                 <Col span={10}>
+                     <Input.TextArea placeholder="10" readOnly={!editMode} value={team.roles} onChange={e => onLevelChange(index, 'roles', e.target.value)} />
+                 </Col>
+                 <Col span={2}>
+                     {editMode &&
+                         <Popconfirm title="Are you sure to delete this level?" onConfirm={() => removeLevel(index)}>
                             <Button type="link" icon={<DeleteOutlined danger />} />
-                        </Popconfirm>
-                    }
-                </Col>
-            </Row>
-            <hr />
-        </>)}
+                         </Popconfirm>
+                     }
+                 </Col>
+             </Row>
+             <hr />
+         </>)}
         {editMode &&
             <Row className='addmore--button'>
                 <Col>
@@ -85,7 +223,7 @@ function TableOrg({ content, editMode, form }) {
                 </Col>
             </Row>
         }
-    </div>
+        </div>;
 }
 
 
